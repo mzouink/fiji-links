113b1000759c65560fde8563a1fe910828c79df1
mzouink <zouinkhi.marwan@gmail.com>
2023-10-17 16:59:31 +0200

    keep only linux stuff

--- src/main/java/sc/fiji/links/linux/LinuxPlugin.java
+++ src/main/java/sc/fiji/links/linux/LinuxPlugin.java
@@ -1,37 +1,29 @@
 package sc.fiji.links.linux;
 
-import org.scijava.links.AbstractLinkHandler;
-import org.scijava.links.LinkHandler;
-import org.scijava.links.Links;
+import org.scijava.console.AbstractConsoleArgument;
+import org.scijava.console.ConsoleArgument;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
-import java.net.URI;
-import java.util.Map;
+import java.util.LinkedList;
 
-@Plugin(type = LinkHandler.class)
-public class LinuxPlugin extends AbstractLinkHandler {
+@Plugin(type = ConsoleArgument.class)
+public class LinuxPlugin extends AbstractConsoleArgument {
     public static final String PLUGIN_NAME = "test";
 
     @Parameter
     private UIService uiService;
 
     @Override
-    public void handle(URI uri) {
-        System.out.println("LinuxPlugin : got handle URI " + uri);
-        if (!supports(uri)) throw new UnsupportedOperationException("" + uri);
-        String st = "Hello ";
-        Map<String, String> query = Links.query(uri);
-        if(query.containsKey("msg"))
-            st+= query.get("msg");
-        uiService.showDialog(st);
+    public void handle(LinkedList<String> linkedList) {
+        System.out.println("LinuxPlugin : got handle URI " );
+      for(String s: linkedList)
+          System.out.println(s);
     }
 
     @Override
-    public boolean supports(URI uri) {
-        System.out.println("LinuxPlugin : check supports URI " + uri);
-
-        return super.supports(uri) && PLUGIN_NAME.equals(Links.operation(uri));
+    public Class<LinkedList<String>> getType() {
+        return super.getType();
     }
 }
